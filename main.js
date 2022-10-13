9b3b841e9d004a7f1a511306fef304c0400ea893
Tonnieboy300 <tonnieboy300@icloud.com>
2022-10-12 23:13:40 -0700

    formatted code into functions

--- main.js
+++ main.js
@@ -1,22 +1,46 @@
 let noun;
-//these constants should equal the amount of words MINUS 1.
-const nounAmount = 2
-const verbAmount = -1
-//get nouns data
-var dataNouns = new XMLHttpRequest();
-dataNouns.open("GET","./nouns.json", false);
-dataNouns.send(null);
+//these constants should equal the number of words in their respective file.
+//this should be the last key + 1
+const nounAmount = 3
+const verbAmount = 3
 
-//select words
-var nounSelection = Math.floor(Math.random()*nounAmount)
+function dataGet(fileName){
+    var data = new XMLHttpRequest();
+    data.open("GET",fileName,false);
+    data.send(null);
+    return data;
+}
 
+//chooses a random word from the noun file
+function nounGen(){
+    //select word
+    var nounSelection = Math.floor(Math.random()*nounAmount);
 
+    //parse word data
+    const nounJSON = JSON.parse(dataNouns.responseText);
 
-//parse word data
-const nounJSON = JSON.parse(dataNouns.responseText);
+    //convert random number to selection
+    var nounSelectionString = nounSelection.toString()
+    return nounJSON[nounSelectionString];
+}
 
-//convert random number to selection
-var nounSelectionString = nounSelection.toString()
+//chooses a random word from the verb file
+function verbGen() {
+    //select word
+    var verbSelection = Math.floor(Math.random()*verbAmount);
 
-console.log(nounSelectionString);
-console.log(nounJSON[nounSelectionString]);
+    //parse word data
+    const verbJSON = JSON.parse(dataVerbs.responseText);
+
+    //convert random number to selection
+    var verbSelectionString = verbSelection.toString();
+    return verbJSON[verbSelectionString];
+}
+
+const dataNouns = dataGet("./nouns.json");
+const dataVerbs = dataGet("./verbs.json");
+
+var noun1 = nounGen();
+var verb = verbGen();
+var noun2 = nounGen();
+console.log(noun1 + " "+ verb + " " + noun2);
